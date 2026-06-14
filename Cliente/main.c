06117593b02a6214e3f8e6701f2691dd91efdd6a
DieguiniCE <l.araya.1@estudiantec.cr>
2026-06-14 10:19:17 -0600

    [UPDATE] Interfaz

--- Cliente/main.c
+++ Cliente/main.c
@@ -1,79 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define WIN32_LEAN_AND_MEAN  //Carga solo un pedazo de windows
+#define NOGDI
+#define NOUSER
 #include <winsock2.h>
+#include <windows.h>
 #include <ws2tcpip.h>
-
-
-#pragma comment(lib, "ws2_32.lib") //Enlaza la librería estática de sockets en Windows
-
+#include "include/raylib.h"
+#pragma comment(lib, "ws2_32.lib")
 #include "constantes.h"
 #include "estructuras.h"
 
+
+DWORD WINAPI EscucharServidor(LPVOID Parametro) {
+    SOCKET SocketServidor = *(SOCKET*)Parametro; //Socket desde el main
+    char BufferRecepcion[1024];
+
+    while (1) {
+        memset(BufferRecepcion, 0, sizeof(BufferRecepcion));
+        int BytesRecibidos = recv(SocketServidor, BufferRecepcion, sizeof(BufferRecepcion), 0);
+
+        if (BytesRecibidos > 0) {
+            printf("Mensaje de Java: %s\n", BufferRecepcion); //Mensaje de java
+        } 
+        else if (BytesRecibidos == 0 || BytesRecibidos == SOCKET_ERROR) {
+            printf("Desconectado del servidor.\n");
+            break;
+        }
+    }
+    return 0;
+}
+
 int main(int argc, char *argv[]) {
     printf("Iniciando Cliente de spaCEinvaders...\n");
-    printf("Configurado para el puerto: %d\n", PUERTO_SERVIDOR);
 
-    nave canon;
-    canon.x = ANCHO_PANTALLA / 2;
-    canon.y = ALTO_PANTALLA - 50;
-    canon.vidas = VIDAS_INICIALES;
-    canon.puntuacion = 0;
+    nave Canon;
+    Canon.x = ANCHO_PANTALLA / 2;
+    Canon.y = ALTO_PANTALLA - 50;
+    Canon.vidas = VIDAS_INICIALES;
+    Canon.puntuacion = 0;
 
-    printf("Jugador listo en posicion X: %d, Y: %d con %d vidas.\n", canon.x, canon.y, canon.vidas);
-
-    // Puntero inicial para la lista de aliens
     NodoAlien* CabezaListaAliens = NULL;
-    
-    WSADATA wsaData; //WinSock
-    int resultadoWSA = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (resultadoWSA != 0) {
-        printf("Error: WSAStartup fallo con error: %d\n", resultadoWSA);
-        return -1;
-    }
 
-    SOCKET SocketCliente;
+    WSADATA wsaData; //Socket
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    SOCKET SocketCliente = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in DireccionServidor;
-    char BufferRecepcion[1024] = {0};
-
-    SocketCliente = socket(AF_INET, SOCK_STREAM, 0);//Socket
-    if (SocketCliente == INVALID_SOCKET) {
-        printf("Error: No se pudo crear el socket. Codigo de error: %d\n", WSAGetLastError());
-        WSACleanup();
-        return -1;
-    }
-    printf("Socket creado exitosamente.\n");
-
     DireccionServidor.sin_family = AF_INET;
     DireccionServidor.sin_port = htons(PUERTO_SERVIDOR); 
-    DireccionServidor.sin_addr.s_addr = inet_addr(IP_SERVIDOR);
+    DireccionServidor.sin_addr.s_addr = inet_addr(IP_SERVIDOR); 
 
-    printf("Intentando conectar al servidor Java en la IP %s...\n", IP_SERVIDOR);
     if (connect(SocketCliente, (struct sockaddr *)&DireccionServidor, sizeof(DireccionServidor)) == SOCKET_ERROR) {
-        printf("Error: La conexion al servidor fallo. Asegurate de que Java este corriendo.\n");
-        closesocket(SocketCliente);
-        WSACleanup();
-        return -1;
+        printf("Advertencia: No se pudo conectar a Java. Abriendo en modo offline.\n");
+    } else {
+        printf("Conectado exitosamente al Servidor!\n");
     }
-    printf("Conectado exitosamente al Servidor!\n");
 
+    CreateThread(NULL, 0, EscucharServidor, &SocketCliente, 0, NULL);//Hilo para escuchar el servidor
+
+    InitWindow(ANCHO_PANTALLA, ALTO_PANTALLA, "spaCEinvaders - Cliente Jugador");//Raylib
+    SetTargetFPS(60); //Estos son los FPS constantes
 
     int JuegoActivo = 1;
 
-    while (JuegoActivo) {
-        memset(BufferRecepcion, 0, sizeof(BufferRecepcion));
+    while (JuegoActivo && !WindowShouldClose()) { //Tocar la X o Esc
+        
+        //CONTROLES
+        if (IsKeyDown(KEY_RIGHT)) Canon.x += 5;
+        if (IsKeyDown(KEY_LEFT)) Canon.x -= 5;
 
-        int BytesRecibidos = recv(SocketCliente, BufferRecepcion, sizeof(BufferRecepcion), 0); //"read()"
+        // B. DIBUJAR
+        BeginDrawing();
+        
+        ClearBackground(BLACK);
+        DrawRectangle(Canon.x, Canon.y, 40, 20, GREEN);//Cañón
+        DrawText("spaCEinvaders", 10, 10, 20, LIGHTGRAY);// Texto en pantalla
+        DrawText(TextFormat("Vidas: %d", Canon.vidas), 10, 40, 20, RED);
 
-        if (BytesRecibidos > 0) {
-            printf("Mensaje del Servidor: %s\n", BufferRecepcion);
-        } else if (BytesRecibidos == 0) {
-            printf("El servidor ha cerrado la conexion.\n");
-            JuegoActivo = 0; 
-        } else {
-            printf("Error leyendo el mensaje del servidor.\n");
-            JuegoActivo = 0;
-        }
+        EndDrawing();
     }
 
     printf("Cerrando el cliente...\n");
